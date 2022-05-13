5ccad971491b7c3036f5531f4ff17b29c4b77f78
Gheiath <ajamghiath@gmail.com>
2022-05-13 10:26:35 +0200

    Req 3 - Pipeline State Done & Tested

--- Phase2_3/source/common/material/pipeline-state.hpp
+++ Phase2_3/source/common/material/pipeline-state.hpp
@@ -40,11 +40,44 @@ namespace our {
 
         // This function should set the OpenGL options to the values specified by this structure
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
-        void setup() const {
-            //TODO: (Req 3) Write this function
+        void setup() const
+        {
+            //TODONE: (Req 3) Write this function
+            if (faceCulling.enabled)
+            {
+                glEnable(GL_CULL_FACE);
+                glCullFace(faceCulling.culledFace);
+                glFrontFace(faceCulling.frontFace);
+            }
+            else
+                glDisable(GL_CULL_FACE);
+
+            if (depthTesting.enabled)
+            {
+                glEnable(GL_DEPTH_TEST);
+                glDepthFunc(depthTesting.function);
+            }
+            else
+                glDisable(GL_DEPTH_TEST);
+
+            if (blending.enabled)
+            {
+                glEnable(GL_BLEND);
+                glBlendEquation(blending.equation);
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                glBlendColor(blending.constantColor[0], blending.constantColor[1], blending.constantColor[2],
+                             blending.constantColor[3]);
+            }
+            else
+                glDisable(GL_BLEND);
+
+            glDepthMask(depthMask);
+            glColorMask(colorMask[0], colorMask[1], colorMask[2], colorMask[3]);
         }
 
-        // Given a json object, this function deserializes a PipelineState structure
+
+
+            // Given a json object, this function deserializes a PipelineState structure
         void deserialize(const nlohmann::json& data);
     };
 
