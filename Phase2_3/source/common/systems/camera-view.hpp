fd542bf6ff1e611f25d7ca918bcfc463d64a18e6
Gheiath <ajamghiath@gmail.com>
2022-06-04 22:16:20 +0200

    more camera views

--- Phase2_3/source/common/systems/camera-view.hpp
+++ Phase2_3/source/common/systems/camera-view.hpp
@@ -23,8 +23,6 @@ namespace our
 
         void update(World* world, float deltaTime) {
 
-            // change the camera view if V is pressed
-            if(!(app->getKeyboard().isPressed(GLFW_KEY_V)))return;
             CameraComponent* camera = nullptr;
             // get the camera
             for(auto entity : world->getEntities())
@@ -39,12 +37,61 @@ namespace our
             // We get a reference to the entity's position and rotation
             glm::vec3& position = entity->localTransform.position;
             glm::vec3& rotation = entity->localTransform.rotation;
+
+            // look left Q key
+            if (app->getKeyboard().isPressed(GLFW_KEY_Q)) 
+                rotation.y = glm::radians(90.0f);
+            // look right E key
+            else if (app->getKeyboard().isPressed(GLFW_KEY_E)) 
+                rotation.y = glm::radians(-90.0f);
+            // look back TAB key
+            else if (app->getKeyboard().isPressed(GLFW_KEY_TAB)) 
+                rotation.y = glm::radians(180.0f);
+            else
+              rotation.y = 0.0f;
             
-            if(app->getKeyboard().isPressed(GLFW_KEY_1))position=glm::vec3(0,3,10);
-            if(app->getKeyboard().isPressed(GLFW_KEY_2))position=glm::vec3(0,8,15);
-            if(app->getKeyboard().isPressed(GLFW_KEY_3))position=glm::vec3(0,2.5,0);
-            if(app->getKeyboard().isPressed(GLFW_KEY_4))position=glm::vec3(-.5,2.5,0);
-            if(app->getKeyboard().isPressed(GLFW_KEY_5))position=glm::vec3(0,2.5,-1);
+            if(app->getKeyboard().isPressed(GLFW_KEY_1))
+            {
+              // normal view
+              position=glm::vec3(0,3,10);
+              rotation=glm::vec3(0,0,0);
+            }
+            if(app->getKeyboard().isPressed(GLFW_KEY_2))
+            {
+              // far a way view
+              position=glm::vec3(0,8,15);
+              rotation=glm::vec3(0,0,0);
+            }
+            if(app->getKeyboard().isPressed(GLFW_KEY_3))
+            {
+              // inside car view
+              position=glm::vec3(0,2.5,0);
+              rotation=glm::vec3(0,0,0);
+            }
+            if(app->getKeyboard().isPressed(GLFW_KEY_4))
+            {
+              // driver seat view
+              position=glm::vec3(-0.9,2.5,0);
+              rotation=glm::vec3(0,0,0);
+            }
+            if(app->getKeyboard().isPressed(GLFW_KEY_5))
+            {
+              // front view
+              position=glm::vec3(0,2.5,-1);
+              rotation=glm::vec3(0,0,0);
+            }
+            if(app->getKeyboard().isPressed(GLFW_KEY_6))
+            {
+              // front of car view
+              position=glm::vec3(0,2.5,-4.3);
+              rotation=glm::vec3(0,0,0);
+            }
+            if(app->getKeyboard().isPressed(GLFW_KEY_7))
+            {
+              // bird's eye view
+              position=glm::vec3(0,34,0);
+              rotation=glm::vec3(glm::radians(-90.0f),0,0);
+            }
 
         }
 
